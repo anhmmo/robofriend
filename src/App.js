d6424941a87db00f2a3b660ab3d1f35acb2a479c
anhmmo <aun@metropolia.fi>
2020-03-25 20:15:58 +0200

    create new component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import CardList from './CardList';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield : ''
     }
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}))
+  }
+
   onSearchChange = (event) => {
     const search = event.target.value.toLowerCase();
-    this.setState({searchfield : search});
+    this.setState({searchfield: search});
   }
   
   render() {
-    const filtedRobots = robots.filter(item => item.name.toLowerCase().includes(this.state.searchfield));
-
-    return (
+    const filtedRobots = this.state.robots.filter(item => item.name.toLowerCase().includes(this.state.searchfield));
+    if(robots.length === 0 ){
+      return <h1>Loading</h1>
+    }
+    else {
+      return (
     
         <div className="tc">
           <h1>Robofriends</h1>
@@ -28,6 +38,8 @@ class App extends Component {
           <CardList robots={filtedRobots} />
         </div>
     );
+    }
+    
   }
 }
 
