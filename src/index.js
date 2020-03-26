e76077e569a493b097348b752c981a4c047329d2
anhmmo <aun@metropolia.fi>
2020-03-26 17:14:32 +0200

    add Provider to index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
 import App from './Container/App';
 import './index.css';
-
 import 'tachyons';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
