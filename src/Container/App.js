ed0d5e135856859b7ec5b696dc91a2c60ea0cc12
anhmmo <aun@metropolia.fi>
2020-03-27 09:12:09 +0200

    remove default props and state

--- src/Container/App.js
+++ src/Container/App.js
@@ -6,41 +6,37 @@ import Scroll from '../Component/Scroll';
 import './App.css';
 import ErrorBoundry from '../Component/ErrorBoundry'
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-    }
-  }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({robots: users}))
+    this.props.onRequestRobots();
   }
   
   render() {
-    const {robots} = this.state;
-    const {searchField, onSearchChange} = this.props;
+  
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     
     const filtedRobots = robots.filter(item => item.name.toLowerCase().includes(searchField));
-    return !robots.length ? <h1>Loading</h1> :
+    return isPending ? <h1>Loading</h1> :
       (
         <div className="tc">
           <h1>Robofriends</h1>
